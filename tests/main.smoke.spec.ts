7007e42a19bfce08169b7fab3c63953803179781
Patryk <kostrzewa_patryk@wp.pl>
2026-04-01 15:01:21 +0200

    test: add smoke tests for login and register pages

--- tests/main.smoke.spec.ts
+++ tests/main.smoke.spec.ts
@@ -7,3 +7,23 @@ test(
   await expect(page).toHaveTitle(/Rolnopol/);
 });
 
+test(
+  'login page should be visible and loaded',
+  { tag: ['@smoke', '@p1', '@auth', '@login'] }, async ({ page }) => {
+  await page.goto('/login.html');
+  await page.waitForLoadState('load');
+  await expect(page).toHaveURL(/login\.html/);
+  await expect(page.locator('body')).toBeVisible();
+  await expect(page.locator('form')).toBeVisible();
+});
+
+test(
+  'register page should be visible and loaded',
+  { tag: ['@smoke', '@p1', '@auth', '@registration'] }, async ({ page }) => {
+  await page.goto('/register.html');
+  await page.waitForLoadState('load');
+  await expect(page).toHaveURL(/register\.html/);
+  await expect(page.locator('body')).toBeVisible();
+  await expect(page.locator('form')).toBeVisible();
+});
+
